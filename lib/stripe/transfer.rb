57779a69f3e6ef2195689416c844a8efc21bbd62
SeniorDevCode <senior.dev.software@gmail.com>
2013-02-21 18:07:06 -0800

    Split out independent parts of Stripe.request and cleanup code

--- lib/stripe/transfer.rb
+++ lib/stripe/transfer.rb
@@ -1,6 +1,5 @@
 module Stripe
   class Transfer < APIResource
     include Stripe::APIOperations::List
-    include Stripe::APIOperations::Create
   end
 end
--- lib/stripe/util.rb
+++ lib/stripe/util.rb
@@ -25,7 +25,6 @@ module Stripe
         'coupon' => Coupon,
         'event' => Event,
         'transfer' => Transfer,
-        'recipient' => Recipient,
         'list' => ListObject
       }
       case resp
