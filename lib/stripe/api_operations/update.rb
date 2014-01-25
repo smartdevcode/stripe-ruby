636d9a61f9d8a98dc9294bf29e279b97d5b6b1e2
SeniorDevCode <senior.dev.software@gmail.com>
2014-01-24 23:37:09 -0800

    whoops, respond_to? cannot be protected

--- lib/stripe/api_operations/update.rb
+++ lib/stripe/api_operations/update.rb
@@ -1,9 +1,8 @@
 module Stripe
   module APIOperations
     module Update
-      def save(opts={}, api_key=nil)
-        api_key ||= @api_key
-        values = serialize_params(self).merge(opts)
+      def save
+        values = serialize_params(self)
 
         if @values[:metadata]
           values[:metadata] = serialize_metadata
@@ -12,8 +11,8 @@ module Stripe
         if values.length > 0
           values.delete(:id)
 
-          response, key = Stripe.request(:post, url, api_key, values)
-          refresh_from(response, key)
+          response, api_key = Stripe.request(:post, url, @api_key, values)
+          refresh_from(response, api_key)
         end
         self
       end
