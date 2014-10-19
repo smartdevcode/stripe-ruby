2130060bcb370f65bf29a4898df5e0dc87a7f2d4
SeniorDevCode <senior.dev.software@gmail.com>
2014-10-19 10:08:10 -0600

    Extract response helpers to module

--- lib/stripe/util.rb
+++ lib/stripe/util.rb
@@ -65,12 +65,12 @@ module Stripe
     def self.symbolize_names(object)
       case object
       when Hash
-        new_hash = {}
+        new = {}
         object.each do |key, value|
           key = (key.to_sym rescue key) || key
-          new_hash[key] = symbolize_names(value)
+          new[key] = symbolize_names(value)
         end
-        new_hash
+        new
       when Array
         object.map { |value| symbolize_names(value) }
       else
