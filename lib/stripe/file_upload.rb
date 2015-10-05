d60a10a9d0681f20a59dd4244150d1af4bb30db8
SeniorDevCode <senior.dev.software@gmail.com>
2015-10-05 11:00:34 -0700

    Bump version to 1.28.0

--- lib/stripe/file_upload.rb
+++ lib/stripe/file_upload.rb
@@ -1,24 +1,22 @@
 module Stripe
   class FileUpload < APIResource
-    include Stripe::APIOperations::Create
-    include Stripe::APIOperations::List
-
     def self.url
       "/v1/files"
-    end
-
-    def self.request(method, url, params={}, opts={})
-      opts = {
-        :api_base => Stripe::uploads_base
-      }.merge(Util.normalize_opts(opts))
-      super
     end
 
     def self.create(params={}, opts={})
       opts = {
         :content_type => 'multipart/form-data',
-      }.merge(Util.normalize_opts(opts))
-      super
+        :api_base => Stripe::uploads_base
+      }.merge(opts)
+      response, opts = request(:post, url, params, opts)
+      Util.convert_to_stripe_object(response, opts)
+    end
+
+    def self.all(filters={}, opts={})
+      opts = {:api_base => Stripe::uploads_base}.merge(opts)
+      response, opts = request(:get, url, filters, opts)
+      Util.convert_to_stripe_object(response, opts)
     end
   end
 end
